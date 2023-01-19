03e028a3fee9ca9b9f486e21f672c27a20fdae22
Azer Khan <azerkkhan@gmail.com>
2023-01-18 19:22:29 -0500

    Update iou.py

--- src/iou.py
+++ src/iou.py
@@ -1,39 +1,3 @@
-<<<<<<< Updated upstream
-import numpy as np
-np.__version__
-def get_iou(boxGround, boxPred):
-    # coordinates of the area of intersection.
-    ix1 = np.maximum(boxGround[0], boxPred[0])
-    iy1 = np.maximum(boxGround[1], boxPred[1])
-    ix2 = np.minimum(boxGround[2], boxPred[2])
-    iy2 = np.minimum(boxGround[3], boxPred[3])
-     
-    # Intersection height and width.
-    i_height = np.maximum(iy2 - iy1 , np.array(0.))
-    i_width = np.maximum(ix2 - ix1 , np.array(0.))
-     
-    area_of_intersection = i_height * i_width
-     
-    # Ground Truth dimensions.
-    gt_height = boxGround[3] - boxGround[1] 
-    gt_width = boxGround[2] - boxGround[0] 
-     
-    # Prediction dimensions.
-    pd_height = boxPred[3] - boxPred[1] 
-    pd_width = boxPred[2] - boxPred[0] 
-     
-    area_of_union = gt_height * gt_width + pd_height * pd_width - area_of_intersection
-    print("Area Ground")
-    print(gt_height * gt_width)
-    print("Area Predict")
-    print(pd_height * pd_width)
-    print("Area Union")
-    print(area_of_union)
-    print("Area Intersect")
-    print(area_of_intersection)
-    iou = area_of_intersection / area_of_union
-     
-=======
 
 from utils import csv_to_dict
 import pandas as pd
@@ -61,7 +25,6 @@ def bb_intersection_over_union(boxA, boxB):
     iou = interArea / float(boxAArea + boxBArea - interArea)
 
     # return the intersection over union value
->>>>>>> Stashed changes
     return iou
 
 def calc_centroid(xMin, xMax, yMin, yMax):
