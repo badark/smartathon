c8410540104089eaf53606aeaf068616a38e6af7
Haidar Khan <haidark@gmail.com>
2023-01-20 12:54:44 -0500

    added infer script and modified dataloader to handle it

--- src/dataset.py
+++ src/dataset.py
@@ -5,51 +5,66 @@ import torch
 import json
 
 class SmartathonImageDataset(torch.utils.data.Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None, training=True):
+      self.training = training
+      if self.training:
         self.img_labels = dict(json.load(open(annotations_file, 'r')))
         self.img_keys = list(self.img_labels.keys())
-        # self.img_labels = pd.read_csv(annotations_file)
-        self.img_dir = img_dir
-        self.transform = transform
-        self.target_transform = target_transform
+      else:
+        self.img_labels = pd.read_csv(annotations_file)
+      self.img_dir = img_dir
+      self.transform = transform
+      self.target_transform = target_transform
 
     def __len__(self):
+      if self.training:
         return len(self.img_keys)
+      else:
+        return len(self.img_labels.index)
 
     def __getitem__(self, idx):
+      if self.training:
         img_key = self.img_keys[idx]
-        img_labels = self.img_labels[img_key]
-        img_path = os.path.join(self.img_dir, img_key)
-        image = Image.open(img_path)
-        # class 0 is reserved for background according to the docs, we have to modify to deal with removing BAD_STREETLIGHT
-        label = [int(label_dict['class']) for label_dict in img_labels]
-        for i,l in enumerate(label):
-          if l < 6:
-            label[i]+=1
+        img_labels = self.img_labels[img_key] 
+      else:
+        img_key = self.img_labels.iloc[idx]['image_path']
+      
+      img_path = os.path.join(self.img_dir, img_key)
+      image = Image.open(img_path)
+      if self.transform:
+        image = self.transform(image)
+      
+      if not  self.training:
+        return image, dict(img_keys=img_key)
+      
+      # class 0 is reserved for background according to the docs, we have to modify to deal with removing BAD_STREETLIGHT
+      label = [int(label_dict['class']) for label_dict in img_labels]
+      for i,l in enumerate(label):
+        if l < 6:
+          label[i]+=1
 
-        # extract the bounding box coordinates
-        boxes = list()
-        areas = list()
-        for label_dict in img_labels:
-          xmax, xmin, ymax, ymin = [label_dict.get(key) for key in ['xmax', 'xmin', 'ymax', 'ymin']]
-          width, height = image.size
-          def adjust_bb(c, lim):
-            if c < 0: return 0
-            if c >= lim: return lim-1
-            return c
-          xmax, xmin, ymax, ymin = adjust_bb(xmax, width+1), adjust_bb(xmin, width), adjust_bb(ymax, height+1), adjust_bb(ymin, height)
-          if xmin == xmax:
-            xmax = xmin+1
-          if ymin == ymax:
-            ymax = ymin+1
-          boxes.append([xmin, ymin, xmax, ymax])
-          areas.append((ymax-ymin)*(xmax-xmin))
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        areas = torch.as_tensor(areas, dtype=torch.float32)
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        label = torch.as_tensor(label, dtype=torch.int64)
-        target = dict(labels=label, boxes=boxes, areas=areas, img_keys=img_key)
-        return image, target
+      # extract the bounding box coordinates
+      boxes = list()
+      areas = list()
+      for label_dict in img_labels:
+        xmax, xmin, ymax, ymin = [label_dict.get(key) for key in ['xmax', 'xmin', 'ymax', 'ymin']]
+        width, height = image.size
+        def adjust_bb(c, lim):
+          if c < 0: return 0
+          if c >= lim: return lim-1
+          return c
+        xmax, xmin, ymax, ymin = adjust_bb(xmax, width+1), adjust_bb(xmin, width), adjust_bb(ymax, height+1), adjust_bb(ymin, height)
+        if xmin == xmax:
+          xmax = xmin+1
+        if ymin == ymax:
+          ymax = ymin+1
+        boxes.append([xmin, ymin, xmax, ymax])
+        areas.append((ymax-ymin)*(xmax-xmin))
+      boxes = torch.as_tensor(boxes, dtype=torch.float32)
+      areas = torch.as_tensor(areas, dtype=torch.float32)
+      
+      if self.target_transform:
+          label = self.target_transform(label)
+      label = torch.as_tensor(label, dtype=torch.int64)
+      target = dict(labels=label, boxes=boxes, areas=areas, img_keys=img_key)
+      return image, target
---
+++ src/infer.py
@@ -0,0 +1,71 @@
+import os
+import torch
+import torch.utils.data as data
+import pandas as pd
+from ignite.engine import Engine, Events
+from torchmetrics.detection.mean_ap import MeanAveragePrecision
+
+from argparse import ArgumentParser
+
+from dataset import SmartathonImageDataset
+from utils import init_model, collate_fn, dict_to_string, write_string_csv
+
+def load_checkpoint(checkpoint_file, model):
+    checkpoint = torch.load(checkpoint_file)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    return model
+
+def main(args):
+    model, transforms = init_model(args)
+    model = load_checkpoint(args.checkpoint_file, model)
+    
+    if os.name == 'nt':
+        img_dir = args.data_dir+'\\resized_images\\'
+        test_data = SmartathonImageDataset(args.data_dir+'\\test.csv', img_dir, transform=transforms, training=False)
+    else:
+        img_dir = args.data_dir+'/resized_images/'
+        test_data = SmartathonImageDataset(args.data_dir+'/test.csv', img_dir, transform=transforms, training=False)
+    
+    test_iterator = data.DataLoader(test_data, batch_size=args.batch_size, collate_fn=collate_fn)
+
+    device = torch.device('cuda')
+    model.to(device)
+    model.eval()
+
+    header = ",".join(['class','image_path', 'name', 'xmax','xmin','ymax','ymin','scores'])
+    out_csv_list = []
+    for batch in test_iterator:
+        with torch.no_grad():
+            images, targets = batch
+            images = list(image.to(device) for image in images)
+            img_keys = [t['img_keys'] for t in targets]
+
+            # - boxes (``FloatTensor[N, 4]``): the predicted boxes in ``[x1, y1, x2, y2]`` format, with
+            # ``0 <= x1 < x2 <= W`` and ``0 <= y1 < y2 <= H``.
+            # - labels (``Int64Tensor[N]``): the predicted labels for each detection
+            # - scores (``Tensor[N]``): the scores of each detection
+            outputs = model(images)
+            outputs = [{k: v.cpu() for k, v in t.items()} for t in outputs]
+            out_csv_list.extend(dict_to_string(img_keys,outputs))
+
+    #write code to create submission file
+    # format cld_ind, filename, cls_name, xmax, xmin, ymax, ymin 
+    write_string_csv(out_csv_list,header, os.path.join(args.output_prefix, 'submission.csv'))
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Process some integers.')
+    parser.add_argument('-m', '--model_type', dest='model_type', 
+        help='type of model to test, see model.py for supported model types')
+    parser.add_argument('--old_classes', dest='old_classes', default=False, action='store_true',
+        help="to run with the old number of classes")
+    parser.add_argument('-c', '--checkpoint_file', dest='checkpoint_file',
+        help='path to checkpoint file')
+    parser.add_argument('-d', '--data_dir', dest='data_dir', 
+        help='root directory of the dataset')
+    parser.add_argument('-b', '--batch_size', dest='batch_size', default=8, type=int,
+        help="batch size during inference")
+    parser.add_argument('-o', '--output_prefix', dest='output_prefix',
+        help="output path for model predictions")
+    args = parser.parse_args()
+    os.makedirs(args.output_prefix, exist_ok = True) 
+    main(args)
